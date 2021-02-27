eaf9031b54dcdb3b677954e759cd329e75a49836
Chad Jay Harrington <MistahWrite@gmail.com>
2021-02-27 11:14:45 -0700

    Completed meal test with 17 passing tests out of 17

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,55 @@
-function nameMenuItem() {
+function nameMenuItem(menuItem) {
+  return "Delicious " + menuItem;
+}
 
+function createMenuItem(menuItemName, menuItemPrice, menuItemType) {
+  var menuItem = {
+    name: menuItemName,
+    price: menuItemPrice,
+    type: menuItemType
+  };
+
+  return menuItem;
+}
+
+function addIngredients(menuIngredient, menuIngredients) {
+  var isUnique = true;
+
+  for (var i = 0; i < menuIngredients.length; i++) {
+    if (menuIngredients[i] === menuIngredient) {
+      isUnique = false;
+    }
+  }
+
+  if (isUnique == true) {
+    menuIngredients.push(menuIngredient);
+  }
+}
+
+function formatPrice(menuItemPrice) {
+  return "$" + menuItemPrice;
+}
+function decreasePrice (menuItemPrice) {
+  return menuItemPrice * 0.90;
+}
+function createRecipe (menuItemTitle, menuItemIngredients, menuItemType) {
+  let recipe = {
+    title: menuItemTitle,
+    ingredients: menuItemIngredients,
+    type: menuItemType
+  };
+
+  return recipe;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
